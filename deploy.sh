2c3b38e40ef0c8e7aa3ffa4d310a9cba1f982332
Loïc Pottier <lpottier@isi.edu>
2021-12-07 14:01:00 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,23 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
+git clone https://github.com/lpottier/deploy-local-k8s.git
+cd deploy-local-k8s
+
+# Hello world
 kubectl create deployment hello --image=k8s.gcr.io/echoserver:1.4
 kubectl expose deployment hello --type=NodePort --port=8080
+
+# Dashboard
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.2.0/aio/deploy/recommended.yaml
 kubectl patch deployment kubernetes-dashboard -n kubernetes-dashboard --type 'json' -p '[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--enable-skip-login"}]'
+# Metrics
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.4.2/components.yaml
 kubectl patch deployment metrics-server -n kube-system --type 'json' -p '[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--kubelet-insecure-tls"}]'
 
+# Update roles
+kubectl apply -f cluster-roles.yaml
+
+kubectl proxy &
+
+echo "Visit http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/"
